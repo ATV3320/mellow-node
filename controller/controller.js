966a42b48ad699b6efa99c670ab86133e706d013
AryanSangwan <aryan.sangwan@antiersolutions.com>
2023-12-18 19:08:01 +0530

    buggy still

--- controller/controller.js
+++ controller/controller.js
@@ -1,48 +1,60 @@
+class ApiController {
+    // Define methods for each exported function, including constructor if needed
 
+    constructor(req, res) {
+        this.req = req;
+        this.res = res;
+    }
 
+    async adminController() {
+        return this.res.send("hellooooo");
+    }
 
-export async function AdminController(req, res){
-   res.send("hellooooo");
+    async aryan() {
+        return this.res.send("mantis kya baate kar raha hai");
+    }
+
+    async hardkaur() {
+        const number = parseInt(this.req.params.number);
+        const double = number * 2;
+        return this.res.send(double.toString());
+    }
+
+    async kuchikuchi() {
+        const data = this.req.body;
+        console.log(data);
+        return this.res.status(200).send({
+            data: {
+                response: data,
+            },
+        });
+    }
+
+    async getCoinGechoPrices() {
+        const data = this.req.body;
+        console.log(data);
+        return this.res.status(200).send({
+            data: {
+                response: data,
+            },
+        });
+    }
+
+    // async getContractData() {
+
+    // }
+
+    // async spitPublicKey() {
+
+    // }
+
+    // async returnJsonResponse() {
+
+    // }
+
+    // async useDB() {
+
+    // }
 }
 
-export async function Aryan(req, res){
-    res.send("mantis kya baate kar raha hai");
-}
-
-export async function hardkaur(req, res){
-    const number = req.params.number;
-    const num1 = parseInt(number);
-    let double = num1*2;
-    const str = double.toString();
-    res.send(str);
-}
-
-export async function kuchikuchi(req,res){
-    const data = req.body;
-    console.log(data);
-    res.status(200).send({
-        data:{
-            response : data,
-        }
-    })
-}
-
-export async function getCoinGechoPrices(req, res) {
-    
-}
-
-export async function getContractData(req, res) {
-
-}
-
-export async function spitPublicKey(req, res) {
-
-}
-
-export async function returnJsonResponse(req, res) {
-
-}
-
-export async function useDB(req, res) {
-
-}
+export default ApiController;
--- routes/director.js
+++ routes/director.js
@@ -1,17 +1,18 @@
 
 import express from 'express';
-import {AdminController, Aryan,getCoinGechoPrices,getContractDAta,hardkaur,kuchikuchi, returnJsonResponse, spitPublicKey, useDB} from'../controller/controller.js';
+// import {AdminController, Aryan,getCoinGechoPrices,getContractDAta,hardkaur,kuchikuchi, returnJsonResponse, spitPublicKey, useDB} from'../controller/controller.js';
+import  ApiController from "../controller/controller.js";
 
 const router = express.Router();
-router.get('/update',AdminController);
-router.get('/Keshav', Aryan);
-router.post('/kaur/:number', hardkaur);
-router.post('/aaru',kuchikuchi)
-router.get('/getCoinPrice', getCoinGechoPrices);
-router.get('/getContractData', getContractData);
-router.get('/getPublicKey/:string', spitPublicKey);
-router.get('/getJSONResponse', returnJsonResponse);
-router.post('/postToDB', useDB);
+router.get('/update', ApiController.adminController);
+router.get('/Keshav', ApiController.aryan);
+router.post('/kaur/:number', ApiController.hardkaur);
+router.post('/aaru', ApiController.kuchikuchi)
+// router.get('/getCoinPrice', ApiController.getCoinGechoPrices);
+// router.get('/getContractData', ApiController.getContractData);
+// router.get('/getPublicKey/:string', ApiController.spitPublicKey);
+// router.get('/getJSONResponse', ApiController.returnJsonResponse);
+// router.post('/postToDB', ApiController.useDB);
 
 
 export default router;
