4806311fb5e13d5ef671e34dc8b605d011190e7d
AryanSangwan <aryan.sangwan@antiersolutions.com>
2023-12-19 17:04:31 +0530

    nw

--- controller/controller.js
+++ controller/controller.js
@@ -1,60 +1,50 @@
-class ApiController {
-    // Define methods for each exported function, including constructor if needed
-
-    constructor(req, res) {
-        this.req = req;
-        this.res = res;
-    }
-
-    async adminController() {
-        return this.res.send("hellooooo");
-    }
-
-    async aryan() {
-        return this.res.send("mantis kya baate kar raha hai");
-    }
-
-    async hardkaur() {
-        const number = parseInt(this.req.params.number);
-        const double = number * 2;
-        return this.res.send(double.toString());
-    }
-
-    async kuchikuchi() {
-        const data = this.req.body;
-        console.log(data);
-        return this.res.status(200).send({
-            data: {
-                response: data,
-            },
-        });
-    }
-
-    async getCoinGechoPrices() {
-        const data = this.req.body;
-        console.log(data);
-        return this.res.status(200).send({
-            data: {
-                response: data,
-            },
-        });
-    }
-
-    // async getContractData() {
-
-    // }
-
-    // async spitPublicKey() {
-
-    // }
-
-    // async returnJsonResponse() {
-
-    // }
-
-    // async useDB() {
-
-    // }
+export async function adminController(req, res) {
+    return this.res.send("hellooooo");
 }
 
-export default ApiController;
+export async function aryan(req, res) {
+    return this.res.send("mantis kya baate kar raha hai");
+}
+
+export async function hardkaur(req, res) {
+    const number = parseInt(this.req.params.number);
+    const double = number * 2;
+    return this.res.send(double.toString());
+}
+
+export async function kuchikuchi(req, res) {
+    const data = this.req.body;
+    console.log(data);
+    return this.res.status(200).send({
+        data: {
+            response: data,
+        },
+    });
+}
+
+export async function getCoinGechoPrices(req, res) {
+    const data = this.req.body;
+    console.log(data);
+    return this.res.status(200).send({
+        data: {
+            response: data,
+        },
+    });
+}
+
+export async function getContractData(req, res) {
+
+}
+
+export async function spitPublicKey(req, res) {
+
+}
+
+export async function returnJsonResponse(req, res) {
+
+}
+
+export async function useDB(req, res) {
+
+}
+
