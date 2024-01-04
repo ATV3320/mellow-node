4ee6089c69a94a1c525cb1a86cf1cad992597962
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2024-01-05 04:27:23 +0530

    resolved, functioning CRUD

--- base/src/server.js
+++ base/src/server.js
@@ -26,10 +26,37 @@ app.get('/getAllProducts', async(req, res)=>{
     }
 })
 
-app.get('/producthunt', async(req, res)=>{
+app.put('/updateProduct/:id', async(req, res)=>{
     try{
-        const {id} = req.params.id;
-        const product = await Product.findOne({id : id});
+        const {id} = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        //if we can not find product in our db
+        if(!product){
+            res.status(404).json({message: `cannot find product with ID ${id
+            }`})
+        }
+        res.status(200).json(product)
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.delete('/destructionof/:id', async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id)
+        if(!product){
+            return res.status(404).json({message:`no product with the id ${id} exists`})
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/producthunt/:id', async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findById(id);
         res.status(200).json(product)
     }catch(error){
         res.status(500).json({message: error.message})
